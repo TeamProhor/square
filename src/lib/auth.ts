6d05cb765e1a5254a13eb4dd2692bbd05761991f
root <root@vmi3278513.contaboserver.net>
2026-08-14 13:05:17 +0200

    update stuffs

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -28,11 +28,6 @@ export const auth = betterAuth({
     enabled: true,
   },
   socialProviders: {
-    github: {
-      clientId: process.env.GITHUB_CLIENT_ID || "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
-      enabled: Boolean(process.env.GITHUB_CLIENT_ID),
-    },
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
